f84f6d88871c895063df1d4b3fa4acd581b26fa4
Arik DeRapak <ariel.safar.tx@gmail.com>
2021-08-21 09:27:47 +0300

    added API to communicate with the Arduino

--- stepper-test-uno.ino
+++ stepper-test-uno.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <A4988.h>
 #include <Ethernet.h>
+#include <EEPROM.h>
 
 // <!-- Stepper motor stuff -->
 #define DIR 5
@@ -13,108 +14,217 @@
 
 byte RPM = 120;
 A4988 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
+double cw_revolutions = 0, ccw_revolutions = 0;
 
-long revolutions = 0;
-short direction = 1;
+enum DIRECTION
+{
+	CW = 1,
+	CCW = -1
+};
 
-byte mac_address[] = {0xDE, 0xA0, 0xB3, 0x5F, 0xFE, 0xED};
-EthernetServer server(80); // Using port 80
+DIRECTION direction = CW;
+
+// <!-- Network stuff -->
+#define PORT 80
+EthernetServer server(PORT);
+EthernetClient client;
+String buffer;
+boolean currentLineIsBlank;
+
+// <!-- Storage addresses -->
+#define CW_REVS_ADDR 100
+#define CCW_REVS_ADDR 110
 
 void setup()
 {
-    init_serial(9600);
-    init_sdcard();
-    init_ethernet(server, mac_address);
-    init_stepper(RPM, MICROSTEPS);
+	byte mac_address[] = {0xDE, 0xA0, 0xB3, 0x5F, 0xFE, 0xED};
+	init_serial(9600);
+	init_server(server, mac_address);
+	init_stepper(RPM, MICROSTEPS);
+
+	cw_revolutions = get_revolutions_storage(CW_REVS_ADDR);
+	ccw_revolutions = get_revolutions_storage(CCW_REVS_ADDR);
 }
 
 void loop()
 {
-    EthernetClient client = server.available();
-    if (client)
-    {
-        boolean currentLineIsBlank = true;
-        String buffer = "";
-        while (client.connected())
-        {
-            if (client.available())
-            {
-                char c = client.read(); // Read from the Ethernet shield
-                buffer += c;            // Add character to string buffer
-                // Client sent request, now waiting for response
-                if (c == '\n' && currentLineIsBlank)
-                {
-                    // Todo: read from file
-                    html_response(client);
+	handle_motor();
+	handle_network();
 
-                    break;
-                }
-                if (c == '\n')
-                {
-                    currentLineIsBlank = true;
-                    buffer = "";
-                }
-                else if (c == '\r')
-                { // Command from webpage
-                    if (buffer.indexOf("GET /?direction=CW") >= 0)
-                        direction = 1;
-                    if (buffer.indexOf("GET /?direction=CCW") >= 0)
-                        direction = -1;
-                }
-                else
-                {
-                    currentLineIsBlank = false;
-                }
-            }
-        }
-        client.stop(); // End server
-    }
+	if (direction == CW && (int)cw_revolutions % 20 == 0 && (int)cw_revolutions == cw_revolutions)
+		save_revolutions_to_storage(CW_REVS_ADDR, (int)cw_revolutions);
 
-    revolutions += 0.5;
-    stepper.rotate(direction * 1 * 180); // in microsteps
-    Serial.println("Revolutions: " + String(floor(revolutions)));
+	if (direction == CCW && (int)ccw_revolutions % 20 == 0 && (int)ccw_revolutions == ccw_revolutions)
+		save_revolutions_to_storage(CCW_REVS_ADDR, (int)ccw_revolutions);
 }
 
-void init_serial(long data_rate)
+// <!-- EEPROM Memory -->
+unsigned int get_revolutions_storage(int addr)
 {
-    Serial.begin(data_rate);
-    Serial.println("Serial started successfully with data rate of " + data_rate);
+	return (EEPROM.read(addr) << 8) + EEPROM.read(addr + 1);
 }
 
-void init_stepper(short rpm, short microsteps)
+void save_revolutions_to_storage(int addr, int revolutions)
 {
-    stepper.begin(rpm, microsteps);
-    Serial.println("Stepper motor initialized successfully, set to " + String(rpm) + " RPM and step ratio of 1:" + String(microsteps));
+	writeUnsignedIntIntoEEPROM(addr, revolutions);
 }
 
-void init_ethernet(EthernetServer server, byte mac_addr[])
+void writeUnsignedIntIntoEEPROM(int address, unsigned int number)
 {
-    Ethernet.begin(mac_addr);
-    server.begin();
-    Serial.println("Ethernet initialized successfully " + Ethernet.localIP());
+	EEPROM.write(address, number >> 8);
+	EEPROM.write(address + 1, number & 0xFF);
 }
 
-void init_sdcard()
+void clean_memory()
 {
-    bool res = 0;
-    do
-    {
-        res = SD.begin(4);
-        if (!res)
-            Serial.println("SD Card failed");
-
-    } while (!res);
-    Serial.println("SD Card initialized successfully");
+	cw_revolutions = 0;
+	ccw_revolutions = 0;
+	writeUnsignedIntIntoEEPROM(CW_REVS_ADDR, 0);
+	writeUnsignedIntIntoEEPROM(CCW_REVS_ADDR, 0);
 }
 
-void html_response(EthernetClient client)
+// <!-- Initializers -->
+void init_serial(unsigned int data_rate)
 {
-    client.println("HTTP/1.1 200 OK"); // HTTP response
-    client.println("Content-Type: text/html");
-    client.println(); // HTML code
+	Serial.begin(data_rate);
+	Serial.println("Serial started successfully with data rate of " + String(data_rate));
+}
 
-    client.print("<center><br><h1>Stepper Motor</h1><br><br><br><FORM>");
-    client.print("<P> <INPUT type=\"submit\" name=\"direction\" value=\"CW\">");
-    client.print("<P> <INPUT type=\"submit\" name=\"direction\" value=\"CCW\">");
-    client.print("</FORM></center>");
+void init_stepper(unsigned short rpm, unsigned short microsteps)
+{
+	stepper.begin(rpm, microsteps);
+	Serial.println("Stepper motor started successfully, set to " + String(rpm) + " RPM and step ratio to 1:" + String(microsteps));
+}
+
+void init_server(EthernetServer server, byte mac_addr[])
+{
+	Ethernet.begin(mac_addr);
+	server.begin();
+	Serial.print("Ethernet initialized successfully: ");
+	Serial.println(Ethernet.localIP());
+}
+
+// <!-- Handle network stuff -->
+
+void send_response(String json, String content_type, String status_code)
+{
+	client.print("HTTP/1.1 ");
+	client.println(status_code);
+	client.print("Content-Type: ");
+	client.println(content_type);
+	client.println();
+
+	client.print(json);
+}
+
+String json(const char *key, const char *value)
+{
+	char buffer[30];
+	sprintf(buffer, "{\"%s\":\"%s\"}", key, value);
+	return String(buffer);
+}
+
+String json(const char *key, const int value)
+{
+	char buffer[30];
+	sprintf(buffer, "{\"%s\":%d}", key, value);
+	return String(buffer);
+}
+
+void handle_motor()
+{
+	if (RPM <= 0)
+	{
+		stepper.setRPM(0);
+		return;
+	}
+	stepper.setRPM(RPM);
+
+	if (direction == 1)
+		cw_revolutions += 0.125;
+	else
+		ccw_revolutions += 0.125;
+
+	stepper.rotate(45 * direction);
+}
+
+void handle_network()
+{
+	client = server.available();
+	if (!client)
+	{
+		return;
+	}
+
+	boolean currentLineIsBlank = true;
+	buffer = "";
+
+	while (client.connected())
+	{
+		if (!client.available())
+		{
+			break;
+		}
+
+		char c = client.read(); // Read from the Ethernet shield
+		buffer += c;			// Add character to string buffer
+		Serial.print(c);
+		// Client sent request, now waiting for response
+		if (c == '\n' && !currentLineIsBlank)
+		{
+			Serial.println();
+			send_response(handle_response(buffer), "application/json", "200 OK");
+			break;
+		}
+		if (c == '\n')
+		{
+			currentLineIsBlank = true;
+			buffer = "";
+		}
+		else if (c == '\r')
+		{
+		}
+		else
+			currentLineIsBlank = false;
+	}
+	client.stop(); // End server
+}
+
+// This is the place to add more abilities
+String handle_response(String buffer)
+{
+	if (buffer.indexOf("POST /?direction=CW") >= 0)
+	{
+		direction = CW;
+		return json("direction", "CW");
+	}
+	if (buffer.indexOf("POST /?direction=CCW") >= 0)
+	{
+		direction = CCW;
+		return json("direction", "CCW");
+	}
+
+	if (buffer.indexOf("POST /?speed=") >= 0)
+	{
+		String tmp = buffer.substring(13, 16);
+		RPM = tmp.toInt();
+		return json("speed", RPM);
+	}
+
+	if (buffer.indexOf("DELETE /revolutions") >= 0)
+	{
+		clean_memory();
+		return json("revolutions", 0);
+	}
+
+	if (buffer.indexOf("GET /speed") >= 0)
+		return json("speed", RPM);
+	if (buffer.indexOf("GET /direction") >= 0)
+		return json("direction", direction == 1 ? "CW" : "CCW");
+	if (buffer.indexOf("GET /revolutions?direction=CW") >= 0)
+		return json("cw_revolutions", cw_revolutions);
+	if (buffer.indexOf("GET /revolutions?direction=CCW") >= 0)
+		return json("ccw_revolutions", ccw_revolutions);
+	if (buffer.indexOf("GET /revolutions") >= 0)
+		return json("revolutions", cw_revolutions + ccw_revolutions);
 }
